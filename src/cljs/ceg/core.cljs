2f9012382c306be560862d898e3f988d72da5a2b
Michael Willis <willismichael@gmail.com>
2016-01-18 06:34:38 -0600

    Double sized pixels, resize works, including updated aspect ratio

--- src/cljs/ceg/core.cljs
+++ src/cljs/ceg/core.cljs
@@ -3,10 +3,7 @@
 
 (def THREE js/THREE)
 
-(.log js/console (aget js/window "innerWidth"))
-(.log js/console (aget js/window "innerHeight"))
-(.log js/console (/ (aget js/window "innerWidth") (aget js/window "innerHeight")))
-
+(defonce canvas (.getElementById js/document "canvas"))
 (defonce scene (new THREE.Scene))
 (defonce cam (new THREE.PerspectiveCamera 75
                   (/ (aget js/window "innerWidth")
@@ -17,7 +14,7 @@
 (defonce geo (new THREE.BoxGeometry 200 200 200))
 (defonce mat (new THREE.MeshBasicMaterial (js-obj "color" 0xff0000 "wireframe" true)))
 (defonce mesh (new THREE.Mesh geo mat))
-(defonce renderer (new THREE.WebGLRenderer))
+(defonce renderer (new THREE.WebGLRenderer (js-obj {"canvas" canvas})))
 
 (defn animate []
   (js/requestAnimationFrame animate)
@@ -28,10 +25,22 @@
     (aset rot "y" (+ y 0.02))
     (.render renderer scene cam)))
 
+(defn resize []
+  (let [screen-width  (aget js/window "innerWidth")
+        screen-height (aget js/window "innerHeight")
+        render-width  (/ screen-width  2)
+        render-height (/ screen-height 2)]
+    (.setSize renderer render-width render-height false)
+    (aset cam "aspect" (/ render-width render-height))
+    (.updateProjectionMatrix cam)))
+
 (defn init!
   []
   (aset (aget cam "position") "z" 1000)
   (.add scene mesh)
-  (.setSize renderer (aget js/window "innerWidth") (aget js/window "innerHeight"))
+  (resize)
+
   (.appendChild (aget js/document "body") (aget renderer "domElement"))
   (animate))
+
+(aset js/window "onresize" resize)
